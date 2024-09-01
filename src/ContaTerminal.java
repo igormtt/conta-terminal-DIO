b7cab49ca8a78a6c2f52369e847970b7d8ed8190
Igor <igorg6149@gmail.com>
2024-09-01 11:32:52 -0300

    Primeiro desafio do bootcamp Java - Claro da DIO

---
+++ src/ContaTerminal.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+
+        Scanner sc = new Scanner(System.in);
+        int numero;
+        String agencia;
+        String nomeCliente;
+        double saldo = 237.48;
+    
+        System.out.println("Por favor, informe qual o seu nome: ");
+        nomeCliente = sc.nextLine();
+
+        System.out.println("Por favor, digite o número da Agência!");
+        agencia = sc.nextLine();
+
+        System.out.println("Por favor, digite o número!");
+        numero = sc.nextInt();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + "conta " + numero + "e seu saldo " + saldo + " já está disponível para saque.");
+    }
+}
